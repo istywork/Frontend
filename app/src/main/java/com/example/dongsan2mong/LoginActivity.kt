c88dca5009f65c4cc8a46837116d11ec9fea56bd
Mingyu0626 <mg010626@naver.com>
2023-05-14 22:04:34 +0900

    modify activity_login and LoginActivity add images

--- app/src/main/java/com/example/dongsan2mong/LoginActivity.kt
+++ app/src/main/java/com/example/dongsan2mong/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.dongsan2mong
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dongsan2mong.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initLayout()
     }
+
+    private fun initLayout() {
+    }
+
+
 }
