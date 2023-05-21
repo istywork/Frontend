51b8921ad98a5127eaf137f4523f132f0c1707f2
Mingyu0626 <mg010626@naver.com>
2023-05-21 19:58:49 +0900

    modify WishlistFragment

--- app/src/main/java/com/example/dongsan2mong/WishlistFragment.kt
+++ app/src/main/java/com/example/dongsan2mong/WishlistFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.dongsan2mong.databinding.FragmentWishlistBinding
 
-class WishlistFragment: Fragment() {
+class WishlistFragment : Fragment() {
+    lateinit var binding: FragmentWishlistBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -14,4 +16,6 @@ class WishlistFragment: Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_wishlist, container, false)
     }
+
+
 }
