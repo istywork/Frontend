d722e6e32d65d4d1e786e9d01097ef14932f549b
Mingyu0626 <mg010626@naver.com>
2023-05-21 23:39:48 +0900

    commit for pull

--- app/src/main/java/com/example/dongsan2mong/MapFragment.kt
+++ app/src/main/java/com/example/dongsan2mong/MapFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.dongsan2mong.databinding.FragmentMapBinding
 
-class MapFragment: Fragment() {
+class MapFragment : Fragment() {
+    lateinit var binding: FragmentMapBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
